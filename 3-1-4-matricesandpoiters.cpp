4d394745b024433378ca9087c1fac5f3816e745d
EmilioRCPantojaLnx <emiliorafael.pantoja02@gmail.com>
2025-08-02 23:39:40 -0300

    matrices with pointers(i think i get it now)

---
+++ 3-1-4-matricesandpoiters.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	int matrix[10][10] = { };
+
+	int *linha, *coluna, *valor;
+
+    valor = &matrix[0][0];
+
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j<10; j++) 
+        {
+            *valor = (i + 1) * (j + 1);
+            valor++;
+
+        }
+    }
+
+
+	for(int i = 0; i < 10; i++) {
+		for(int j = 0; j < 10; j++) {
+			cout.width(4);
+			cout << matrix[i][j];
+		}
+		cout << endl;
+	}
+	return 0;
+}
